02053b4ccd0843d1456767124acce27354897732
spencercap <cappiello.spencer@gmail.com>
2021-03-29 23:28:10 +0200

    updated vite build config for easy gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  build: {
+    outDir: 'docs', // easy github pages
+  },
+  base: './' // helps github pages
 })
